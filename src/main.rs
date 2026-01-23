db31b1deaf67c4bcda1f8602cf87b2ee506bbfc6
Sammeet Koli <sammeet29@gmail.com>
2026-01-22 23:15:43 -0800

    Remove Cli and add dialoguer to allow user to talk with the application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use dialoguer::Input;
+
 fn main() {
-    println!("Hello, world!");
+  loop {
+    
+    let command: String = Input::new()
+      .with_prompt("Enter a command:")
+      .interact_text()
+      .unwrap();
+
+    println!("Command: {}", command);
+
+    let mut exit = false;
+
+    match command.as_str() {
+      "exit" => exit = true,
+      _ => ,
+    }
+
+    // if exit 
+    // {
+    break;
+    // }
+  }
 }
